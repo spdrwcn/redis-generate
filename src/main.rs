2b9e5ed689d2d4dea304c578d4f1aae2e2046d06
本子画师三岁半 <157886909+spdrwcn@users.noreply.github.com>
2024-05-28 23:56:04 +0800

    1.0.0

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
-use indicatif::{ProgressBar, ProgressStyle};
 use rand::Rng;
-use redis::Commands;
 use serde_json::json;
-use std::error::Error;
+use std::time::Instant;
+use clap::{App, Arg};
+
+fn get_matches() -> clap::ArgMatches<'static> {
+    let matches = App::new("redis-generate")
+        .version("1.0.0")
+        .author("h13317136163@163.com")
+        .about("redis数据生成工具")
+        .arg(
+            Arg::with_name("ip")
+                .short("i")
+                .long("ip")
+                .value_name("IP_ADDRESS")
+                .help("Redis数据库地址")
+                .default_value("redis://127.0.0.1:6379/0"),
+        )
+        .arg(
+            Arg::with_name("num")
+                .short("n")
+                .long("num")
+                .value_name("number of pairs")
+                .help("生成数量")
+                .default_value("10000"),
+        )
+        .get_matches();
+    matches
+}
+
+
 fn generate_random_mac() -> String {
     let mut rng = rand::thread_rng();
     format!(
@@ -16,47 +42,33 @@ fn generate_random_mac() -> String {
     )
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let client = redis::Client::open("redis://127.0.0.1/")?;
+fn generate_key_value_pairs<'a>(num_pairs: usize) -> impl Iterator<Item = (String, String)> + 'a {
+    (1..=num_pairs).map(move |i| {
+        (
+            format!("key{}", i),
+            json!({
+                "wired_mac": generate_random_mac(),
+                "wireless_mac": generate_random_mac(),
+                "bluetooth_mac": generate_random_mac(),
+            }).to_string(),
+        )
+    })
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let start_time = Instant::now();
+    let matches = get_matches();
+    let ip_address = matches.value_of("ip").unwrap();
+    let number = matches.value_of("num").unwrap().parse::<usize>()?;
+    let client = redis::Client::open(ip_address)?;
     let mut con = client.get_connection()?;
-
-    let pb = ProgressBar::new(1_000_000);
-    pb.set_style(
-        ProgressStyle::default_bar()
-            .template(
-                "{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len} ({eta})",
-            )?
-            .progress_chars("#>-"),
-    );
-
-    let mut errors = Vec::new();
-
-    for i in 0..1_000_000 {
-        let key = format!("key{}", i);
-        let value = json!({
-            "wired_mac": generate_random_mac(),
-            "wireless_mac": generate_random_mac(),
-            "bluetooth_mac": generate_random_mac()
-        });
-
-        if let Err(error) = con.set::<&str, &str, String>(&key, &*value.to_string()) {
-            errors.push(format!(
-                "Unable to set value in Redis for key {}: {}",
-                key, error
-            ));
-        } else {
-            pb.inc(1);
-        }
+    
+    let mut pipe = redis::pipe();
+    for (key, value) in generate_key_value_pairs(number) {
+        pipe.set(key, value);
     }
-
-    pb.finish_with_message("");
-
-    if !errors.is_empty() {
-        eprintln!("The following errors occurred:");
-        for error in &errors {
-            eprintln!("{}", error);
-        }
-    }
-
+    pipe.query(&mut con)?;
+    let elapsed_time = start_time.elapsed();
+    println!("用时： {:?}", elapsed_time);
     Ok(())
 }
